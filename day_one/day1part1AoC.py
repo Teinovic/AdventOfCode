e90c71375baeb6ffdc86727dca2416d000fb1c5b
Teinovic <63501035+Teinovic@users.noreply.github.com>
2021-01-05 00:42:26 +0100

    Update day1part1AoC.py

--- day_one/day1part1AoC.py
+++ day_one/day1part1AoC.py
@@ -1,10 +1,3 @@
 input = [int(x) for x in open('inputday1part1.txt').read().strip('\n').splitlines()]
 
-
-solution = []
-for num in input:
-    for num_two in input:
-        if num + num_two == 2020:
-            solution.append(num * num_two)
-
-print(set(solution))
+print({x * y for x in input for y in input if x + y == 2020})
